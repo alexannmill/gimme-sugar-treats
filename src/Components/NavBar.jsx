28b7affc4bad5c2c3950e2cc269547761499662f
alexezpt <alexannmill@gmail.com>
2023-08-07 15:34:12 -0700

    Adding material icons, responsive nav bar - change to burger menu, drop down for xs screens

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,26 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import {
+	AppBar,
+	Toolbar,
+	Typography,
+	Button,
+	Menu,
+	MenuItem,
+	IconButton,
+	Box,
+} from '@mui/material';
+import { List } from '@mui/icons-material';
 
 const NavBar = () => {
+	const [anchorEl, setAnchorEl] = useState(null);
+	const isMenuOpen = Boolean(anchorEl);
+
+	const handleMenuOpen = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleMenuClose = () => {
+		setAnchorEl(null);
+	};
+
+	const renderMenu = (
+		<Menu anchorEl={anchorEl} open={isMenuOpen} onClose={handleMenuClose}>
+			<MenuItem component={Link} to='/'>
+				Home
+			</MenuItem>
+			<MenuItem component={Link} to='/about'>
+				About
+			</MenuItem>
+			<MenuItem component={Link} to='/contact'>
+				Contact
+			</MenuItem>
+			<MenuItem component={Link} to='/menu'>
+				Menu
+			</MenuItem>
+		</Menu>
+	);
+
 	return (
 		<AppBar position='static'>
 			<Toolbar>
 				<Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
 					GIMMIE SUGAR TREATS
 				</Typography>
-				<Button color='inherit' component={Link} to='/'>
-					Home
-				</Button>
-				<Button color='inherit' component={Link} to='/about'>
-					About
-				</Button>
-				<Button color='inherit' component={Link} to='/contact'>
-					Contact
-				</Button>
-				<Button color='inherit' component={Link} to='/menu'>
-					Menu
-				</Button>
+				<IconButton
+					size='large'
+					edge='end'
+					color='inherit'
+					aria-label='menu'
+					aria-haspopup='true'
+					onClick={handleMenuOpen}
+					sx={{ display: { xs: 'block', md: 'none' } }}
+				>
+					<List />
+				</IconButton>
+				{renderMenu}
+				<Box sx={{ display: { xs: 'none', md: 'block' } }}>
+					<Button color='inherit' component={Link} to='/'>
+						Home
+					</Button>
+					<Button color='inherit' component={Link} to='/about'>
+						About
+					</Button>
+					<Button color='inherit' component={Link} to='/contact'>
+						Contact
+					</Button>
+					<Button color='inherit' component={Link} to='/menu'>
+						Menu
+					</Button>
+				</Box>
 			</Toolbar>
 		</AppBar>
 	);
