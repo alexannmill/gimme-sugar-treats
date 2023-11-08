7f89aad1f854407f41feb7c723341267e6d4e3cf
alexezpt <alexannmill@gmail.com>
2023-11-08 08:50:29 -0800

    Grid layout for contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,9 +6,9 @@ import {
 	Button,
 	Box,
 	FormControl,
+	Grid,
 } from '@mui/material';
 import theme from '../Styles/theme';
-
 
 const classes = {};
 const Contact = () => {
@@ -83,65 +83,68 @@ const Contact = () => {
 		// 		backgroundRepeat: 'no-repeat',
 		// 	}}
 		// >
-		<Container maxWidth='sm'>
+		<Grid container sx={{ padding: '10%' }}>
 			<br />
-			<Typography variant='h4' gutterBottom>
-				Contact Us
-			</Typography>
-			{!submitted ? (
-				<>
-					<Typography>
-						Have a question or want to place an order?
-						<br />
-						Please fill out the form below!
-					</Typography>
+			<Grid
+				item
+				xs={12}
+				sm={6}
+				sx={{ alignSelf: 'center', justifyContent: 'center' }}
+			>
+				<Typography variant='h4' gutterBottom>
+					Contact Us
+				</Typography>
+
+				<Typography>
+					Have a question or want to place an order?
 					<br />
-					<Typography variant='p' gutterBottom>
-						* $20 delivery, MINIMUM 3 days for an order
-					</Typography>
-					<form onSubmit={sendEmail} method='POST'>
-						<TextField
-							variant='outlined'
-							label='Name'
-							name='name'
-							value={formData.name}
-							onChange={handleChange}
-							fullWidth
-							margin='normal'
-							required
-						/>
-						<TextField
-							label='Email'
-							name='email'
-							type='email'
-							inputProps={{ type: 'email' }}
-							value={formData.email}
-							onChange={handleChange}
-							fullWidth
-							margin='normal'
-							required
-						/>
-						<TextField
-							label='Message'
-							name='message'
-							value={formData.message}
-							onChange={handleChange}
-							fullWidth
-							margin='normal'
-							multiline
-							rows={4}
-							required
-						/>
-						<Button type='submit' variant='contained' color='primary'>
-							Submit
-						</Button>
-					</form>
-				</>
-			) : (
-				<></>
-			)}
-		</Container>
-		// </Box>
+					Please fill out the form below!
+				</Typography>
+				<br />
+				<Typography variant='p' gutterBottom>
+					* $20 delivery, MINIMUM 3 days for an order
+				</Typography>
+			</Grid>
+			<Grid item xs={12} sm={6}>
+				<form onSubmit={sendEmail} method='POST'>
+					<TextField
+						variant='outlined'
+						label='Name'
+						name='name'
+						value={formData.name}
+						onChange={handleChange}
+						fullWidth
+						margin='normal'
+						required
+					/>
+					<TextField
+						label='Email'
+						name='email'
+						type='email'
+						inputProps={{ type: 'email' }}
+						value={formData.email}
+						onChange={handleChange}
+						fullWidth
+						margin='normal'
+						required
+					/>
+					<TextField
+						label='Message'
+						name='message'
+						value={formData.message}
+						onChange={handleChange}
+						fullWidth
+						margin='normal'
+						multiline
+						rows={4}
+						required
+					/>
+					<Button type='submit' variant='contained' color='primary'>
+						Submit
+					</Button>
+				</form>
+			</Grid>
+		</Grid>
 	);
 };
 
