31343ef3b199f3b09cbc8252b5cc01d7fcec34b1
alexezpt <alexannmill@gmail.com>
2023-12-03 15:07:07 -0800

    Dynamically change # of columns in gallery based on screen size

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { ImageList, ImageListItem, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Box, ImageList, ImageListItem, Typography } from '@mui/material';
 
+// import all images in Images directory
 function importAll(r) {
 	let images = {};
 	r.keys().forEach((item, index) => {
@@ -9,24 +10,45 @@ function importAll(r) {
 	return images;
 }
 
+// require all imported images
 const images = importAll(
 	require.context('../Images', false, /\.(png|jpe?g|svg)$/)
 );
 
 const Gallery = () => {
-	console.log('Img:', images);
+	const [numberOfColumns, setNumberOfColumns] = useState(2);
+
+	const handleResize = () => {
+		const screenSize = window.screen.width;
+		// dynamically change number of columns based on screen size
+		const screenDivide = Math.round(screenSize / 400);
+		//ensure at least 2 columns at all times
+		const numColumns = screenDivide < 2 ? 2 : screenDivide;
+		setNumberOfColumns(numColumns);
+	};
+
+	useEffect(() => {
+		// event listener for window resize
+		window.addEventListener('resize', handleResize);
+
+		// cleanup: Remove the event listener on component unmount
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
+	//process image data to img list formate
 	const itemData = Object.entries(images).map((e) => ({
 		title: e[0],
 		img: e[1],
 	}));
-	console.log('itemData:', itemData);
 
 	return (
-		<div>
+		<Box p='5%'>
 			<Typography variant='h1' color='secondary' gutterBottom align='center'>
 				GALLERY
 			</Typography>
-			<ImageList variant='masonry' cols={3} gap={8}>
+			<ImageList variant='masonry' cols={numberOfColumns} gap={8}>
 				{itemData.map((item) => (
 					<ImageListItem key={item.img}>
 						<img
@@ -38,7 +60,7 @@ const Gallery = () => {
 					</ImageListItem>
 				))}
 			</ImageList>
-		</div>
+		</Box>
 	);
 };
 
