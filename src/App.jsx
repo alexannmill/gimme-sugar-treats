e38dfe2db49a1f46247b1ff483a691563587e621
alexezpt <alexannmill@gmail.com>
2023-12-06 08:03:02 -0800

    github pages router fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import theme from './Styles/theme';
 import { ThemeProvider } from '@mui/material';
@@ -7,19 +7,19 @@ import AnimatedRoutes from './Context/AnimatedRoutes';
 
 const App = () => {
 	return (
-		<Router basename=''>
-			<ThemeProvider theme={theme}>
-				<main
-					style={{
-						backgroundColor: '#e5d2c0',
-						minHeight: '100vh',
-					}}
-				>
-					<NavBar />
-					<AnimatedRoutes />
-				</main>
-			</ThemeProvider>
-		</Router>
+		// <BrowserRouter basename=''>
+		<ThemeProvider theme={theme}>
+			<main
+				style={{
+					backgroundColor: '#e5d2c0',
+					minHeight: '100vh',
+				}}
+			>
+				<NavBar />
+				<AnimatedRoutes />
+			</main>
+		</ThemeProvider>
+		// </BrowserRouter>
 	);
 };
 
